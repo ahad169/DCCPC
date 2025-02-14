6067b69b57cbd17a49ccf34085aaa51d71cbc1a5
Abdullah Al Ahad Bhuiyan <abdullahalahadbhuiyan@gmail.com>
2025-02-14 18:36:03 +0600

    ami mori nai

--- Practice/loop/o.cpp
+++ Practice/loop/o.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 
 void solve() {
-		int k, s; cin >> k >> s;
-		long long ans = 0;
-		for(int i = 0; i <= k; i++) {
-			for(int j = 0; j <= k; j++) {
-				int z = s - i - j;
-            	if (z >= 0 && z <= k) {
-                	ans++;
-            	}
-			}
+	int k, s; cin >> k >> s;
+	long long ans = 0;
+	for(int i = 0; i <= k; i++) {
+		for(int j = 0; j <= k; j++) {
+			int z = s - i - j;
+           		if (z >= 0 && z <= k) {
+                		ans++;
+            		}
 		}
-
-		cout << ans << "\n";
+	}
+	cout << ans << "\n";
 }
 
 int main() {
